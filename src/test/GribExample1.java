195231c94c7922ede8beab60a921c7d6615ea3a7
Andrew Spiteri <andrew.spiteri@um.edu.mt>
2017-11-21 15:10:48 +0100

    updated GribExample1

--- src/test/GribExample1.java
+++ src/test/GribExample1.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
+import mt.edu.um.cf2.jgribx.GribCodes;
 import mt.edu.um.cf2.jgribx.GribFile;
 import mt.edu.um.cf2.jgribx.JGribX;
 import mt.edu.um.cf2.jgribx.Logger;
@@ -34,11 +35,12 @@ public class GribExample1
         Logger.setLoggingMode(Logger.LoggingMode.CONSOLE);
         Logger.setLevel(Logger.WARNING);
         
-//        String gribFilename = "test/gfsanl_3_20160324_0600_000.grb";
-        String gribFilename = "test/gfsanl_3_20170512_0000_000.grb2";
+        String gribFilename = "test/gfsanl_3_20160324_0600_000.grb";
+//        String gribFilename = "test/gfsanl_3_20170512_0000_000.grb2";
         
         // Prepare format for reference times
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss 'UTC'");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         
         try
@@ -48,36 +50,53 @@ public class GribExample1
             // Get info
             int nRecords = gribFile.getRecordCount();
             List<Calendar> refDates = gribFile.getReferenceTimes();
+            List<Calendar> forecastDates = gribFile.getForecastTimes();
             List<String> params = gribFile.getParameterCodes();
             int[] centreIds = gribFile.getCentreIDs();
             int[] processIds = gribFile.getProcessIDs();
             
+            // Print out generic GRIB file info
+            System.out.println("---------------------------------------");
+            System.out.println("Reading file: " + gribFilename);
+            System.out.println("GRIB Edition: " + gribFile.getEdition());
+            System.out.println("Records successfully read: " + nRecords + " of "
+                    + (nRecords + gribFile.getRecordsSkippedCount()));
             System.out.println("---------------------------------------");
             
             // Print out originating centre info
-            System.out.print("Weather centre(s): ");
+            System.out.print("Weather Centre(s): ");
             for (int i = 0; i < centreIds.length; i++)
             {
-                System.out.print(centreIds[i]);
+                System.out.print(centreIds[i]
+                        + " [" + GribCodes.getCentreName(centreIds[i]) + "]");
                 if (i != centreIds.length - 1) System.out.print(",");
             }
             System.out.println();
             
-            // Print out originating centre info
-            System.out.print("Generating process(es): ");
+            // Print out generating process info
+            System.out.print("Generating Process(es): ");
             for (int i = 0; i < processIds.length; i++)
             {
-                System.out.print(processIds[i]);
+                System.out.print(processIds[i]
+                        + " [" + GribCodes.getProcessName(processIds[i]) + "]");
                 if (i != processIds.length - 1) System.out.print(",");
             }
             System.out.println();
             
-            System.out.println("GRIB file contains " + nRecords + " records");
-            System.out.println("References times: ");
+            // Get forecast times
+            System.out.println("Forecast Time(s): ");
+            for (Calendar date : forecastDates)
+            {
+                System.out.println("\t" + sdf.format(date.getTime()));
+            }
+            
+            // Get reference time
+            System.out.println("Reference Time: ");
             for (Calendar date : refDates)
             {
                 System.out.println("\t" + sdf.format(date.getTime()));
             }
+            
             System.out.println("Data: ");
             for (String paramCode : params)
             {
