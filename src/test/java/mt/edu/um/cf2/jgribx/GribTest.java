a97b2f735dd825ab3d70dda25e6610f1ea0f0d89
spidru <andrew.spiteri@outlook.com>
2020-03-17 22:59:33 +0100

    Add testGrib1Gfs3

--- src/test/java/mt/edu/um/cf2/jgribx/GribTest.java
+++ src/test/java/mt/edu/um/cf2/jgribx/GribTest.java
@@ -2,14 +2,24 @@ package mt.edu.um.cf2.jgribx;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GribTest
 {
+    protected void setUp()
+    {
+        // Prepare format for reference times
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss 'UTC'");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
     @Test
     public void testVersion()
     {
@@ -18,6 +28,28 @@ public class GribTest
         String version = JGribX.getVersion();
         Matcher match = pattern.matcher(version);
         assertTrue("Version string format is valid: " + version, match.find());
+    }
+
+    @Test
+    public void testGrib1Gfs3() throws NotSupportedException, IOException, NoValidGribException
+    {
+        final String FILENAME = "CF2_20150706_092531.grb";
+
+        // Define expected data
+        final int N_RECORDS_EXPECTED = 304;
+        final int GRIB_EDITION = 1;
+        final int[] WEATHER_CENTRES = {7};
+        final int[] GENERATING_PROCESSES = {81, 96};
+
+        //try
+        {
+            GribFile gribFile = new GribFile("src/test/resources/" + FILENAME);
+
+            assertEquals("Records read successfully", N_RECORDS_EXPECTED, gribFile.getRecordCount());
+            assertEquals("GRIB edition", GRIB_EDITION, gribFile.getEdition());
+            assertArrayEquals("Weather centres", WEATHER_CENTRES, gribFile.getCentreIDs());
+            assertArrayEquals("Generating processes", GENERATING_PROCESSES, gribFile.getProcessIDs());
+        }
     }
 
 }
