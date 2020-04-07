254e5150e5f277f82f3013a02f32bac3c54c24c5
spidru <andrew.spiteri@outlook.com>
2020-04-07 23:12:23 +0200

    Add GRIB2 file to GribTest

--- src/test/java/mt/edu/um/cf2/jgribx/GribTest.java
+++ src/test/java/mt/edu/um/cf2/jgribx/GribTest.java
@@ -1,11 +1,15 @@
 package mt.edu.um.cf2.jgribx;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,11 +18,16 @@ import static org.junit.Assert.*;
 
 public class GribTest
 {
-    protected void setUp()
+    @BeforeClass
+    public static void setUpBeforeClass()
     {
         // Prepare format for reference times
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss 'UTC'");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        // Configure logging
+        Logger.setLoggingMode(Logger.LoggingMode.CONSOLE);
+        JGribX.setLoggingLevel(Logger.INFO);
     }
 
     @Test
@@ -42,8 +51,6 @@ public class GribTest
         final int[] WEATHER_CENTRES = {7};
         final int[] GENERATING_PROCESSES = {81, 96};
 
-        Logger.setLoggingMode(Logger.LoggingMode.CONSOLE);
-        JGribX.setLoggingLevel(5);
         URL url = GribTest.class.getResource(FILENAME);
         System.out.println("Path to file: " + url);
         GribFile gribFile = new GribFile(url.openStream());
@@ -52,6 +59,44 @@ public class GribTest
         assertEquals("GRIB edition", GRIB_EDITION, gribFile.getEdition());
         assertArrayEquals("Weather centres", WEATHER_CENTRES, gribFile.getCentreIDs());
         assertArrayEquals("Generating processes", GENERATING_PROCESSES, gribFile.getProcessIDs());
+    }
+
+    @Test
+    public void testGrib2Gfs3() throws IOException, NoValidGribException, NotSupportedException
+    {
+        /* TODO
+         * Unsupported parameter: D:LAND_SURFACE C:4 N:2
+         * Unsupported parameter: D:LAND_SURFACE C:0 N:2
+         * Unsupported parameter: D:LAND_SURFACE C:0 N:192
+         * Unsupported parameter: D:LAND_SURFACE C:0 N:201
+         * Unsupported parameter: D:LAND_SURFACE C:3 N:203
+         * Unsupported parameter: D:METEOROLOGICAL C:6 N:201
+         * Unsupported parameter: D:METEOROLOGICAL C:7 N:192
+         * Unsupported parameter: D:METEOROLOGICAL C:7 N:6
+         * Unsupported parameter: D:METEOROLOGICAL C:7 N:7
+         * Skipping GRIB record 274 (Record does not have a valid GRIB header)
+         * Unsupported level of type 104
+         * Skipping GRIB record 337 (Record does not have a valid GRIB header)
+         * Unsupported parameter: D:OCEANOGRAPHIC C:2 N:0
+         * Unsupported level of type 109
+         */
+        final String FILENAME = "/gfsanl_3_20170512_0000_000.grb2";
+
+        // Define expected data
+        final int N_RECORDS_EXPECTED = 0;
+        final int EDITION = 2;
+        final int[] WEATHER_CENTRES = {7};
+        final int[] GENERATING_PROCESSES = {81};
+        List<Calendar> refTimes = new ArrayList<Calendar>();
+        refTimes.add(new GregorianCalendar(2017, Calendar.MAY, 12, 0, 0, 0));
+
+        URL url = GribTest.class.getResource(FILENAME);
+        GribFile file = new GribFile(url.openStream());
+
+        assertEquals("GRIB edition", EDITION, file.getEdition());
+        assertEquals("Reference time(s)", file.getReferenceTimes(), refTimes);
+        assertArrayEquals("Weather centres", WEATHER_CENTRES, file.getCentreIDs());
+        assertArrayEquals("Generating processes", GENERATING_PROCESSES, file.getProcessIDs());
     }
 
 }
